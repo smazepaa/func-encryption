4a0f1ae10330694b38ab5f8ace82bf97daa5a42c
smazepaa <smazepa@kse.ord.ua>
2023-11-04 22:32:57 +0200

    added c++ dll file

--- encryption.py
+++ encryption.py
@@ -1,26 +1,31 @@
-# def encrypt(text, key):
-#     return ''.join(chr(ord(c) + key) for c in text)
+import ctypes
 
-# def encrypt_full(text, number):
-#     result = ""
-#     for i in text:
-#         ascii_value = ord(i)
-#         new_ascii_value = ascii_value + number
-#         new_character = chr(new_ascii_value)
-#         result += new_character
-#     return result
+# Load the DLL
+my_lib = ctypes.CDLL('C:/Users/sofma/source/func-encryption.git/encryption.dll')
 
-# def decrypt(text, key):
-#     return ''.join(chr(ord(c) - key) for c in text)
+# Declare the argument types and return type of your functions
+my_lib.encrypt.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_int]
+my_lib.encrypt.restype = ctypes.c_char_p
+my_lib.decrypt.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_int]
+my_lib.decrypt.restype = ctypes.c_char_p
+my_lib.free_memory.argtypes = [ctypes.c_char_p]
+my_lib.free_memory.restype = None
 
+# Use the functions
+text = b"your text"
+number = 1
+length = len(text)
 
-encrypt = lambda text, key: ''.join(chr((ord(c) + key) % 128) for c in text)
-decrypt = lambda text, key: ''.join(chr((ord(c) - key) % 128) for c in text)
+encrypted_text = my_lib.encrypt(text, number, length)
+print("Encrypted text:", encrypted_text.decode())
 
+decrypted_text = my_lib.decrypt(encrypted_text, number, length)
+print("Decrypted text:", decrypted_text.decode())
 
-# command - function to encrypt/decrypt
-# get_text - function to get from console/file
-# output_text - function to get from console/file
+# Free the memory allocated by the DLL
+my_lib.free_memory(encrypted_text)
+my_lib.free_memory(decrypted_text)
+
 
 def execute(command, get_text, key, output_text):
     encrypted_or_decrypted_text = command(get_text, key)
