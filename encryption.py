70ca272f383f5989f95e28fca5bd1beb3dbb3315
smazepaa <smazepa@kse.ord.ua>
2023-11-04 23:47:24 +0200

    fixed dll and imported functions

--- encryption.py
+++ encryption.py
@@ -11,22 +11,11 @@ my_lib.decrypt.restype = ctypes.c_char_p
 my_lib.free_memory.argtypes = [ctypes.c_char_p]
 my_lib.free_memory.restype = None
 
-# Use the functions
-text = b"your text"
-number = 1
-length = len(text)
 
-encrypted_text = my_lib.encrypt(text, number, length)
-print("Encrypted text:", encrypted_text.decode())
-
-decrypted_text = my_lib.decrypt(encrypted_text, number, length)
-print("Decrypted text:", decrypted_text.decode())
-
-# Free the memory allocated by the DLL
-my_lib.free_memory(encrypted_text)
-my_lib.free_memory(decrypted_text)
+encrypt = lambda text, key: my_lib.encrypt(text.encode(), key, len(text)).decode()
+decrypt = lambda text, key: my_lib.decrypt(text.encode(), key, len(text)).decode()
 
 
 def execute(command, get_text, key, output_text):
-    encrypted_or_decrypted_text = command(get_text, key)
-    output_text(encrypted_or_decrypted_text)
+    result = command(get_text, key)
+    output_text(result)
